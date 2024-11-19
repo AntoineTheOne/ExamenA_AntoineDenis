790543a0e31f95b8551b4888f4b03da024cea67a
AntoineTheOne <Antoine.Denis2@outlook.com>
2024-11-19 09:16:47 -0500

    scene pour changer de scene fini

---
+++ Assets/Scripts/LevelManagement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManagement : MonoBehaviour
+{
+
+
+
+
+
+public void ChangementScene(){
+   SceneManager.LoadScene("ExamenA_02");
+}
+
+
+
+
+}
